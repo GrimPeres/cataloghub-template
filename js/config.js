0c07c62d0c9d32c611270329a8b274f603be38a3
GrimPeres <grim_alexandre@hotmail.com>
2026-07-05 00:04:00 +0100

    0

---
+++ js/config.js
@@ -0,0 +1,29 @@
+const config = {
+    empresa: {
+        nome: "A Minha Empresa",
+        slogan: "Catálogos Digitais",
+        corprincipal: "#2e2e2e",
+        corsecundaria: "#6d6d6d",
+        logo:"img/MDKimport_Logo.png"
+    },
+
+    catalogos: [
+        {
+            titulo: "Catálogo Principal",
+            pdf: "pdf/catalogo.pdf"
+        }
+    ],
+
+    contactos: {
+        website: "https://www.exemplo.pt",
+        email: "geral@exemplo.pt",
+        phone: "+351 912345678"
+    },
+
+    redes: {
+        facebook: "facebook.com/nunoperes",
+        instagram: "instagram.com/nuno_ap_peres"
+
+    }
+
+};
