242b83932bdc0cea5cf194f69cdf84bc54cf2161
GrimPeres <grim_alexandre@hotmail.com>
2026-07-08 11:53:20 +0100

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -20,5 +20,45 @@ const catalogos = [
         ativo: true
 
     }
+        {
+        id: 2,
+
+        titulo: "Catálogo MDK",
+
+        descricao: "MDK IMPORT",
+
+        capa: "img/catalogo1.jpg",
+
+        pdf: "pdf/catalogo.pdf",
+
+        destaque: true,
+
+        categoria: "Produtos",
+
+        idioma: "pt",
+
+        ativo: true
+
+    }
+        {
+        id: 3,
+
+        titulo: "Catálogo Layout",
+
+        descricao: "Catalogo Layoutcriativo",
+
+        capa: "img/catalogo1.jpg",
+
+        pdf: "pdf/catalogo.pdf",
+
+        destaque: true,
+
+        categoria: "Produtos",
+
+        idioma: "pt",
+
+        ativo: true
+
+    }
 
 ];
